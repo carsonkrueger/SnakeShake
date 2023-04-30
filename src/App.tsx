6f351e74ee61ee8720a06b04388737e91ac81684
carsonkrueger <crkruege@gmail.com>
2023-04-30 09:19:08 -0600

    snake movement mostly complete

--- src/App.tsx
+++ src/App.tsx
@@ -14,75 +14,122 @@ type snakeTile = {
 };
 
 function App() {
-  const BOARD_SIZE = useRef(15);
+  const BOARD_WIDTH = useRef(15);
   const BOARD_CENTER = useRef(
-    Math.floor((BOARD_SIZE.current * BOARD_SIZE.current) / 2)
+    Math.floor((BOARD_WIDTH.current * BOARD_WIDTH.current) / 2)
   );
+  const MAX_BOARD_SIZE = useRef(BOARD_WIDTH.current * BOARD_WIDTH.current);
   const SNAKE_SPEED = useRef(500);
   const CUR_DIRECTION = useRef(direction.RIGHT);
-
-  const [snake, setSnake] = useState(new Array<number>());
+  const [snake, setSnake] = useState([BOARD_CENTER.current]);
   const [board, setBoard] = useState([
-    ...Array(BOARD_SIZE.current * BOARD_SIZE.current),
+    ...Array(BOARD_WIDTH.current * BOARD_WIDTH.current),
   ]);
 
   useEffect(() => {
-    setSnake([BOARD_CENTER.current]);
+    function handleKeyDown(e: KeyboardEvent) {
+      switch (e.key) {
+        case "w":
+        case "ArrowUp":
+          CUR_DIRECTION.current = direction.UP;
+          break;
+        case "d":
+        case "ArrowRight":
+          CUR_DIRECTION.current = direction.RIGHT;
+          break;
+        case "s":
+        case "ArrowDown":
+          CUR_DIRECTION.current = direction.DOWN;
+          break;
+        case "a":
+        case "ArrowLeft":
+          CUR_DIRECTION.current = direction.LEFT;
+          break;
+      }
+    }
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => document.removeEventListener("keydown", handleKeyDown);
   }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log([...snake]);
+      move();
+      console.log(snake);
     }, SNAKE_SPEED.current);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [snake]);
 
-  const isSnakeIdx = (idx: number) => {
+  function isSnakeIdx(idx: number): boolean {
     return snake.includes(idx);
-  };
+  }
 
-  const upIdx = (idx: number) => {
-    // if (idx < 0)
-    //   return (BOARD_SIZE.current * BOARD_SIZE.current + idx)
-    return idx - BOARD_CENTER.current;
-  };
+  function upIdx(idx: number): number {
+    return idx - BOARD_WIDTH.current;
+  }
 
-  const rightIdx = (idx: number) => {
+  function rightIdx(idx: number): number {
     return idx + 1;
-  };
+  }
 
-  const downIdx = (idx: number) => {
-    return idx - BOARD_CENTER.current;
-  };
+  function downIdx(idx: number): number {
+    return idx + BOARD_WIDTH.current;
+  }
 
-  const leftIdx = (idx: number) => {
+  function leftIdx(idx: number): number {
     return idx - 1;
-  };
+  }
 
-  const move = () => {
-    let newSnake = [...snake];
-    newSnake.shift();
-    let headPos: number = snake.at(snake.length - 1);
+  function isInBounds(idx: number): boolean {
     switch (CUR_DIRECTION.current) {
       case direction.UP:
-        let newIdx = upIdx(headPos);
-        newSnake.push(newIdx);
+        if (idx < 0) return false;
+        return true;
+      case direction.RIGHT:
+        if (idx % BOARD_WIDTH.current === 0) return false;
+        return true;
+      case direction.DOWN:
+        if (idx > MAX_BOARD_SIZE.current) return false;
+        return true;
+      case direction.LEFT:
+        if (idx % BOARD_WIDTH.current === BOARD_WIDTH.current - 1) return false;
+        return true;
+    }
+  }
+
+  function move(): void {
+    let headPos: number = ensure(snake.at(snake.length - 1));
+    let newIdx = 0;
+
+    const newSnake = [...snake];
+    newSnake.shift();
+
+    switch (CUR_DIRECTION.current) {
+      case direction.UP:
+        newIdx = upIdx(headPos);
         break;
       case direction.RIGHT:
         newIdx = rightIdx(headPos);
-        newSnake.push(newIdx);
         break;
       case direction.DOWN:
         newIdx = downIdx(headPos);
-        newSnake.push(newIdx);
         break;
       case direction.LEFT:
         newIdx = leftIdx(headPos);
-        newSnake.push(newIdx);
         break;
     }
-  };
+    if (isInBounds(newIdx)) {
+      newSnake.push(newIdx);
+      setSnake(() => newSnake);
+    }
+  }
+
+  function ensure<T>(argument: T | undefined | null): T {
+    if (argument === undefined || argument === null)
+      throw new TypeError("Invalid type match");
+    return argument;
+  }
 
   return (
     <div className="flex flex-wrap justify-center items-center w-160 h-160">
